de32b1998ba5b6224e3e2ddec4692e580b8ee48a
sanchitbhateja123 <73834597+sanchitbhateja123@users.noreply.github.com>
2021-01-23 03:27:51 -1000

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,18 @@
+class Ground{
+    constructor(x,y,w,h){
+        var options={
+            isStatic:true
+        }
+        this.body=Bodies.rectangle(x,y,w,h,options)
+
+        World.add(world,this.body)
+        this.w=w
+        this.h=h
+
+    }
+display(){
+    var pos=this.body.position
+    rectMode(CENTER)
+rect(pos.x,pos.y,this.w,this.h)
+}
+}
